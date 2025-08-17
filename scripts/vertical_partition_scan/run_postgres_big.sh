f85f27716596a8c999b09c8c9c65b53f40dbe9fc
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-08-17 10:05:37 +0530

    Create run_postgres_big.sh

---
+++ scripts/vertical_partition_scan/run_postgres_big.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+output_file="$SCRIPT_DIR/../results/postgres_big_vp_scan.txt"
+database_name="generated2"      #change name
+
+echo "-----Without Vertical Partitioning-----" > "$output_file"
+
+echo "---Access All Fields---" >> "$output_file"
+bash ../base_postgres.sh "$database_name" "SELECT * FROM account;" >> "$output_file"
+
+echo "---Access Partial Fields---" >> "$output_file"
+bash ../base_postgres.sh "$database_name" "SELECT id, balance FROM account;" >> "$output_file"
+
+echo "-----With Vertical Partitioning-----" >> "$output_file"
+
+echo "---Access All Fields---" >> "$output_file"
+bash ../base_postgres.sh "$database_name" "Select account1.id, balance, homeaddress from account1, account2 where account1.id = account2.id;" >> "$output_file"
+
+echo "---Access Partial Fields---" >> "$output_file"
+bash ../base_postgres.sh "$database_name" "SELECT id, balance FROM account1;" >> "$output_file"
