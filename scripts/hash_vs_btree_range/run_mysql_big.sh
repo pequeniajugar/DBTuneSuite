d6476b86ea1a5e3c774e54ff4b290d3fa750f1da
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-08-17 10:46:00 +0530

    Create run_mysql_big.sh

---
+++ scripts/hash_vs_btree_range/run_mysql_big.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+database_name="employees_index" # change name
+output_file="$SCRIPT_DIR/../results/mysql_big_hash_vs_btree_range.txt"
+
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "ALTER TABLE employees ENGINE = MEMORY;"
+
+echo "-----No Index-----" > "$output_file"
+bash "$SCRIPT_DIR/../base_mysql.sh" "$database_name" "SELECT * FROM employees WHERE lat BETWEEN 150 AND 160;" >> "$output_file"
+
+echo "-----Hash Index-----" >> "$output_file"
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "CREATE INDEX idx_longitude_hash ON employees (longitude) USING HASH;"
+bash "$SCRIPT_DIR/../base_mysql.sh" "$database_name" "SELECT * FROM employees FORCE INDEX (idx_longitude_hash) WHERE longitude BETWEEN 150 AND 160;" >> "$output_file"
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "DROP INDEX IF EXISTS idx_longitude_hash;"
+
+echo "-----B+ Tree Index-----" >> "$output_file"
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "CREATE INDEX idx_longitude_btree ON employees (longitude) USING BTREE;"
+bash "$SCRIPT_DIR/../base_mysql.sh" "$database_name" "SELECT * FROM employees FORCE INDEX (idx_longitude_btree) WHERE longitude BETWEEN 150 AND 160;" >> "$output_file"
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "DROP INDEX IF EXISTS idx_longitude_btree;"
+
+bash "$SCRIPT_DIR/../configure_mysql.sh" "$database_name" "ALTER TABLE employees ENGINE = InnoDB;"
