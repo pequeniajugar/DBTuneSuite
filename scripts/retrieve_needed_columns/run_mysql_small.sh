9cc3421772cbe1fd660ddb0f52458aa7e600ab16
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-11-20 21:22:56 -0500

    Create run_mysql_small.sh

---
+++ scripts/retrieve_needed_columns/run_mysql_small.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+output_file="$SCRIPT_DIR/../results/mysql_small_retrieve_needed_columns.txt"
+database_name="tpch_small"      #change name
+
+echo "-----Fetch All Columns-----" > "$output_file"
+bash ../base_mysql.sh "$database_name" "SELECT * FROM lineitem;" >> "$output_file"
+
+echo "-----Fetch Needed Columns-----" > "$output_file"
+bash ../base_mysql.sh "$database_name" "SELECT l_orderkey, l_partkey, l_suppkey, l_shipdate, l_commitdate FROM lineitem;" >> "$output_file"
