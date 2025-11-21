5adf6f8ef75c1cf57132041ea6b7a1a0a1ad120e
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-11-20 21:37:23 -0500

    Add script to run MySQL queries for salary analysis
    This script executes SQL queries to find employees with the highest salary in their department using different methods: correlated subquery, temporary table, and WITH clause.

---
+++ scripts/correlated_subquery/run_mysql_small.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+output_file="$SCRIPT_DIR/../results/mysql_small_correlated_subquery.txt"
+database_name="student_techdept_small"      #change name
+
+echo "-----Correlated Subquery-----" > "$output_file"
+bash ../base_mysql.sh "$database_name" "SELECT ssnum FROM employee e1 WHERE salary = (SELECT max(salary) FROM employee e2 WHERE e2.dept = e1.dept);" >> "$output_file"
+
+echo "-----Using Temporary Table-----" > "$output_file"
+bash ../base_mysql.sh "$database_name" "SELECT e1.ssnum FROM employee e1 JOIN ( SELECT dept, MAX(salary) AS bigsalary FROM employee GROUP BY dept) e2 ON e1.dept = e2.dept AND e1.salary = e2.bigsalary;" >> "$output_file"
+
+echo "-----Using WITH Clause-----" > "$output_file"
+bash ../base_mysql.sh "$database_name" "WITH max_salary_per_dept AS ( SELECT dept, MAX(salary) AS bigsalary FROM employee GROUP BY dept ) SELECT e1.ssnum FROM employee e1 JOIN max_salary_per_dept m ON e1.dept = m.dept AND e1.salary = m.bigsalary;" >> "$output_file"
