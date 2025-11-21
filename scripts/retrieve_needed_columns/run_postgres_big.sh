9f4b4560792833baa06eceee1ead2af02420a161
amani-agrawal <50449831+amani-agrawal@users.noreply.github.com>
2025-11-20 21:25:30 -0500

    Add script to retrieve needed columns from PostgreSQL

---
+++ scripts/retrieve_needed_columns/run_postgres_big.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+output_file="$SCRIPT_DIR/../results/postgres_big_retrieve_needed_columns.txt"
+database_name="tpch_big"      #change name
+
+echo "-----Fetch All Columns-----" > "$output_file"
+bash ../base_postgres.sh "$database_name" "SELECT * FROM lineitem;" >> "$output_file"
+
+echo "-----Fetch Needed Columns-----" > "$output_file"
+bash ../base_postgres.sh "$database_name" "SELECT l_orderkey, l_partkey, l_suppkey, l_shipdate, l_commitdate FROM lineitem;" >> "$output_file"
